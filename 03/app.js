95bf8a7dca01a97d78f0b56f8cd92db0e53825c9
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2024-02-29 14:32:27 +0900

    Create app.js

---
+++ 03/app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button");
+const text = document.getElementById("text");
+
+button.addEventListener("click", async () => {
+	text.textContent = await window.openDialog();
+})
