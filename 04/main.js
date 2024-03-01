6603d4daf9f2a28b3f53a448270d69cbe161cd55
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2024-03-01 10:35:34 +0900

    Create main.js

---
+++ 04/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path')
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 400,
+		title: 'マイアプリ',
+		// webPreferencesでウェブページの機能の設定
+		webPreferences: {
+			// nodeIntegrationがtrueでレンダラープロセスがNode.jsの機能を利用できる
+			//nodeIntegration: true,
+			// contextIsolationがtrueでメインプロセスとレンダラープロセスのJavaScriptコンテキストを分離する
+			//contextIsolation: false,
+			preload: path.join(__dirname, 'preload.js'),
+		}
+	})
+
+	ipcMain.handle('open-dialog', async (_e, _arg) => {
+		return dialog
+			// ファイル選択ダイアログを表示する
+			.showOpenDialog(win, {
+				properties: ['openFile'],
+			})
+			.then((result) => {
+				// キャンセルボタンが押された時
+				if (result.canceled) return '';
+
+				// 選択されたファイルの絶対パスを返す
+				return result.filePaths[0];
+			})
+			.catch((err) => console.error(err));
+	})
+
+	//win.webContents.openDevTools({ mode: 'detach'})
+	win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+	createWindow()
+})
